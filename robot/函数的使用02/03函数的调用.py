37c981411c88f9e3a5af5233647bde055db77d1f
seak <274882401@qq.com>
2020-03-15 22:50:49 +0800

    打印，函数def的基本使用1

---
+++ robot/函数的使用02/03函数的调用.py
@@ -0,0 +1,11 @@
+'''
+作者hello=项目：
+题目：
+作用：
+备注：
+'''
+
+from robot.函数的使用02.deff import hello
+
+
+hello()
---
+++ robot/函数的使用02/def2.py
@@ -0,0 +1,13 @@
+'''
+作者：seak
+时间：
+项目：
+题目：
+作用：
+备注：
+'''
+import deff
+
+from robot.函数的使用02.deff import hello
+
+hello()
